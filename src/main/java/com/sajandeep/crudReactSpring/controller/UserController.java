c6059e81d1b3c2f1e9647bac1fb29a6164613afe
Sajandeep Singh <sajanjohal49@gmail.com>
2023-03-20 19:51:49 -0400

    fix crossOrigin

--- src/main/java/com/sajandeep/crudReactSpring/controller/UserController.java
+++ src/main/java/com/sajandeep/crudReactSpring/controller/UserController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
